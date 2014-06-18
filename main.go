f81215ed73e0895c9ac2b563d33a71e919103bbb
Levi Cook <levicook@gmail.com>
2014-06-18 12:10:08 -0600

    minimizing example

--- main.go
+++ main.go
@@ -6,31 +6,24 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/levicook/go-detect"
 	_ "github.com/lib/pq"
 )
 
 var (
-	port        string
-	databaseUrl string
-	database    *sql.DB
+	port     string
+	database *sql.DB
 )
 
 func init() {
-	port = detect.String(os.Getenv("PORT"), "5001")
-	databaseUrl = detect.String(os.Getenv("DATABASE_URL"), "") // todo is there a sensible default for this?
+	port = os.Getenv("PORT")
 
-	db, err := sql.Open("postgres", databaseUrl)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	panicIf(err)
 	database = db
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		for _, env := range os.Environ() {
-			fmt.Fprintln(w, env)
-		}
-
 		var pgPid int
 		panicIf(database.QueryRow("SELECT pg_backend_pid()").Scan(&pgPid))
 		fmt.Fprintf(w, "pg_backend_pid=%v", pgPid)
