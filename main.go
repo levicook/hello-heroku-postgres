bed5f2816d51d7d07e0a1bb7bd7dc7a6940959db
Levi Cook <levicook@gmail.com>
2014-06-18 10:41:23 -0600

    base postgres connectivity

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/levicook/go-detect"
+	_ "github.com/lib/pq"
 )
 
-var port string
+var (
+	port        string
+	databaseUrl string
+	database    *sql.DB
+)
 
 func init() {
 	port = detect.String(os.Getenv("PORT"), "5001")
+	databaseUrl = detect.String(os.Getenv("DATABASE_URL"), "") // todo is there a sensible default for this?
+
+	db, err := sql.Open("postgres", databaseUrl)
+	panicIf(err)
+	database = db
 }
 
 func main() {
@@ -19,9 +30,20 @@ func main() {
 		for _, env := range os.Environ() {
 			fmt.Fprintln(w, env)
 		}
+
+		var pgPid int
+		panicIf(database.QueryRow("SELECT pg_backend_pid()").Scan(&pgPid))
+
+		for _, env := range os.Environ() {
+			fmt.Fprintf(w, "pg_backend_pid=%v", pgPid)
+		}
 	})
 
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	panicIf(http.ListenAndServe(":"+port, nil))
+}
+
+func panicIf(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
